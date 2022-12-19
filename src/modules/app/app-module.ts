7da4ffa4f33ea943f00bd19c9ebe0c82dbcdf7af
Đức Thạch <ducthach.1401@gmail.com>
2022-12-19 18:04:57 +0700

    remove pg

--- src/modules/app/app-module.ts
+++ src/modules/app/app-module.ts
@@ -1,29 +1,19 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import app from 'src/config/app';
 import { JwtAuthGuard } from '../auth/app/jwt/jwt-auth-guard';
 import { AuthModule } from '../auth/auth-module';
 import { AppController } from './app-controller';
 import { AppService } from './app-service';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import database from 'src/config/database';
 import fcm from '../fcm/config/fcm';
 import { FcmModule } from '../fcm/fcm-module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [app, database, fcm],
+      load: [app, fcm],
     }),
     AuthModule,
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) =>
-        configService.get<TypeOrmModuleOptions>(
-          'database',
-        ) as TypeOrmModuleOptions,
-    }),
     FcmModule,
   ],
   controllers: [AppController],
